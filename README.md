0195af81049ee95f303bb360a29fb30eb3342ff3
victoryang00 <yangyiwei2000@gmail.com>
2023-04-04 21:24:43 -0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# CSE210 Programming Language Solution
---
+++ hw1/.Makefile.d
@@ -0,0 +1,9 @@
+BasicsTest.vo BasicsTest.glob BasicsTest.v.beautified BasicsTest.required_vo: BasicsTest.v Basics.vo
+BasicsTest.vio: BasicsTest.v Basics.vio
+BasicsTest.vos BasicsTest.vok BasicsTest.required_vos: BasicsTest.v Basics.vos
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vio: Basics.v 
+Basics.vos Basics.vok Basics.required_vos: Basics.v 
+Preface.vo Preface.glob Preface.v.beautified Preface.required_vo: Preface.v 
+Preface.vio: Preface.v 
+Preface.vos Preface.vok Preface.required_vos: Preface.v 
